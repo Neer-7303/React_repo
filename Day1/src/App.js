366df60c13f65939c24bd052b3e8f0cacbb0bd30
Neer-7303 <53005830+Neer-7303@users.noreply.github.com>
2021-09-28 20:53:42 +0530

    Add files via upload

---
+++ Day1/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Title from './Title';
+
+function App() {
+  return (
+    <><div><h1>This is App Component ......</h1></div><Title /></>
+  );
+}
+
+export default App;
